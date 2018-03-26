1b35016d5bec8351ebd61bd20a810ce76571c063
Hector Vasquez <hvasquezdev@gmail.com>
2018-03-25 22:47:33 -0400

    Modificaciones a los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 
 // Components
-import { PagesComponent } from './pages/pages.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { NotFound404Component } from './not-found404/not-found404.component';
+import { NotFound404Component } from './shared/not-found404/not-found404.component';
 import { RegisterComponent } from './register/register.component';
 
 const appRoutes: Routes = [
-  {path: '', component: PagesComponent, children: [
-    {path: 'dashboard', component: DashboardComponent},
-    {path: 'progress', component: ProgressComponent},
-    {path: 'graficas1', component: Graficas1Component},
-    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  ]},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: '**', component: NotFound404Component}
